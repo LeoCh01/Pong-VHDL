26f153110acd4e4b0fec8524620b0bb6e6e7e6a6
Leo Cheng <83481110+LeoCh01@users.noreply.github.com>
2024-10-24 14:53:53 -0400

    template

--- VideoGame.vhd
+++ VideoGame.vhd
@@ -33,6 +33,24 @@ architecture Behavioral of VideoGame is
 
   type game_state is (START, RUN, FIN);
   signal current_state : game_state := START;
+  type vec2 is array (31 downto 0, 25 downto 0) of INTEGER;
+  signal grid : array_2d := (
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    (0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0),
+    (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0),
+    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+  );
 ---------------------------------------------------------
 -- port maps
 ---------------------------------------------------------
@@ -60,7 +78,52 @@ architecture Behavioral of VideoGame is
         fps <= fps + 1;
       end if;
     end if;
+  end process;
 
+  -- hpos counter
+  process(clk)
+    begin
+      if (clk'Event and clk='1') then
+        if (pos = HD + HFP + HSP + HBP) then
+          hpos <= hpos + 1;
+        else
+          hpos = 0;
+        end if;
+      end if;
+  end process;
+
+  -- horizontal synch
+  process(clk)
+    begin
+      if (clk'Event and clk='1') then
+        if ((pos <= HD + HFP) || (hpos >= HD + HFP + HSP)) then
+          hsync <= '1';
+        else
+          hsync = '0';
+        end if;
+      end if;
+  end process;
+
+  -- vpos counter
+  -- vertical synch
+
+  -- video_on process
+  process(clk)
+    begin
+      if (clk'Event and clk='1') then
+        if (hpos <= HD and vpos <= VD) then
+          video_on <= '1';
+        else
+          video_on = '0';
+        end if;
+      end if;
+  end process;
+
+  -- draw
+  process(clk)
+    begin
+      if (clk'Event and clk='1') then
+      end if;
   end process;
 
 end Behavioral;
