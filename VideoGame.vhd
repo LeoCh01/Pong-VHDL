4accddeba9354e07eb9cea3fed8e3c4fca03bf26
Leo Cheng <83481110+LeoCh01@users.noreply.github.com>
2024-11-07 16:07:42 -0500

    ball logic test

--- VideoGame.vhd
+++ VideoGame.vhd
@@ -36,13 +36,21 @@ architecture Behavioral of VideoGame is
 ---------------------------------------------------------
   signal col : std_logic_vector(3 downto 0);
   constant block_size : integer := 32;
-  signal bx : integer := 0;
-  signal by : integer := 0;
-  signal bx_dir : integer := 0;
-  signal by_dir : integer := 0;
   signal hpos : integer := 0;
   signal vpos : integer := 0;
+  signal video_strb : std_logic := '0';
   signal clk2 : std_logic;
+
+  -- ball
+  constant br : integer := 7;
+  signal bx : integer := 320;
+  signal by : integer := 240;
+  signal bx_dir : integer := -1;
+  signal by_dir : integer := 1;
+  signal btli : integer := 0;
+  signal btri : integer := 0;
+  signal bbli : integer := 0;
+  signal bbri : integer := 0;
 
   type game_state is (START, RUN, FIN);
   signal current_state : game_state := START;
@@ -53,14 +61,14 @@ architecture Behavioral of VideoGame is
   signal grid : vec2 := (
     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
     (0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0),
+    (0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
+    (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
-    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
@@ -116,25 +124,58 @@ architecture Behavioral of VideoGame is
 		
       if (vpos = (480 + 10 + 2 + 33)) then
         vpos <= 0;
+        video_strb <= '1';
       elsif (hpos = (640 + 16 + 96 + 48)) then
         vpos <= vpos + 1;
+        video_strb <= '0';
       end if;
 
-      gx <= hpos / block_size;
-      gy <= vpos / block_size;
-      if (gx <= 20 and gy <= 15) then
-        col <= std_logic_vector(to_unsigned(grid(gy, gx), 4));
-      else 
-        col <= "1111";
-      end if;
 
-      case game_state is
+      if ((bx - br) <= hpos and hpos <= (bx + br) and (by - br) <= vpos and vpos <= (by + br)) then 
+        col <= "0100";
+      else
+        gx <= hpos / block_size;
+        gy <= vpos / block_size;
+
+        if (gx <= 20 and gy <= 15) then
+          col <= std_logic_vector(to_unsigned(grid(gy, gx), 4));
+        else 
+          col <= "1111";
+        end if;
+      end if;
+    end if;
+  end process;
+  
+  process(video_strb)
+  begin
+    if (video_strb'Event and video_strb='1') then
+      case current_state is
         when START =>
+          bx <= 320;
+          by <= 240;
+          current_state <= RUN;
+
         when RUN =>
+          bx <= bx + bx_dir;
+          by <= by + by_dir;
+
+          btli <= grid((by - br) / block_size, (bx - br) / block_size);
+          btri <= grid((by - br) / block_size, (bx + br) / block_size);
+          bbli <= grid((by + br) / block_size, (bx - br) / block_size);
+          bbri <= grid((by + br) / block_size, (bx + br) / block_size);
+
+          if ((btli = 1 and bbli = 1) or (btri = 1 and bbri = 1)) then
+            bx_dir <= -bx_dir;
+				bx <= bx + 4 * bx_dir;
+          end if;
+
+          if ((btli = 1 and btri = 1) or (bbli = 1 and bbri = 1)) then
+            by_dir <= -by_dir;
+				by <= by + 4 * by_dir;
+          end if;
+
         when FIN =>
       end case;
     end if;
-
   end process;
-
 end Behavioral;
