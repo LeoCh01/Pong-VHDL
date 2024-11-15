204a67c6ad7f4e2c8227b47f51db07c294bb031c
Leo Cheng <83481110+LeoCh01@users.noreply.github.com>
2024-11-15 17:13:33 -0500

    styling and collision fixes

--- VideoGame.vhd
+++ VideoGame.vhd
@@ -25,7 +25,7 @@ architecture Behavioral of VideoGame is
   component color_gen is
     port (
       clk : in std_logic;
-      col : in std_logic_vector(2 downto 0);
+      col : in std_logic_vector(11 downto 0);
       Rout : out std_logic_vector(7 downto 0);
       Bout : out std_logic_vector(7 downto 0);
       Gout : out std_logic_vector(7 downto 0)
@@ -35,31 +35,31 @@ architecture Behavioral of VideoGame is
 ---------------------------------------------------------
 -- signals
 ---------------------------------------------------------
-  signal col : std_logic_vector(2 downto 0);
+  signal col : std_logic_vector(11 downto 0);
   constant block_size : integer := 32;
   signal hpos : integer := 0;
   signal vpos : integer := 0;
   signal video_strb : std_logic := '0';
   signal clk2 : std_logic := '0';
-  signal winner : std_logic := '0';
+  signal winner : integer := 1;
   signal sleep : integer := 0;
+  signal test : std_logic := '0';
 
   -- ball
   constant br : integer := 7;
-  constant vel : integer := 2;
-  constant offset : integer := 4;
+  constant vel : integer := 3;
   signal bx : integer := 320;
   signal by : integer := 240;
-  signal bx_dir : integer := 2;
-  signal by_dir : integer := 2;
+  signal bx_dir : integer := vel;
+  signal by_dir : integer := vel;
   signal is_col : integer := 0;
   
   -- paddle
   constant pw : integer := 5;
   constant ph : integer := 40;
-  signal p1x : integer := 75;
+  constant p1x : integer := 75;
   signal p1y : integer := 240;
-  signal p2x : integer := 565;
+  constant p2x : integer := 565;
   signal p2y : integer := 240;
 
   type game_state is (START, RUN, FIN, GONE);
@@ -74,12 +74,12 @@ architecture Behavioral of VideoGame is
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
     (2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2),
-    (2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2),
     (2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2),
     (2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2),
     (2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2),
-    (2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2),
-    (2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 2),
+    (2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2),
+    (2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2),
+    (2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
     (0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0),
     (0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0),
@@ -88,21 +88,21 @@ architecture Behavioral of VideoGame is
 
   signal is_face : integer := 0;
   signal grid_face : vec2 := (
-    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1),
-    (1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1),
-    (1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+    (2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2),
+    (2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2),
+    (2, 2, 2, 2, 2, 3, 3, 3, 2, 2, 2, 2, 2, 3, 3, 3, 2, 2, 2, 2),
+    (2, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2),
+    (2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2),
+    (2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2),
+    (2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2),
+    (2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2),
+    (2, 2, 2, 3, 2, 2, 3, 3, 3, 2, 2, 3, 2, 2, 3, 3, 3, 2, 2, 2),
+    (2, 2, 2, 3, 2, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 2, 3, 2, 2, 2),
+    (2, 2, 2, 3, 2, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 2, 3, 2, 2, 2),
+    (2, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2, 3, 2, 2, 2),
+    (2, 2, 2, 2, 2, 3, 3, 3, 2, 2, 2, 2, 2, 3, 3, 3, 2, 2, 2, 2),
+    (2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2),
+    (2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
   );
   signal cell : integer := 0;
 
@@ -143,6 +143,7 @@ architecture Behavioral of VideoGame is
       
       if (((480 + 10) <= vpos) and (vpos < (480 + 10 + 2))) then
         vsync <= '0';
+        test <= '1';
       else
         vsync <= '1';
       end if;
@@ -164,14 +165,14 @@ architecture Behavioral of VideoGame is
 
       if ((bx - br) <= hpos and hpos <= (bx + br) and (by - br) <= vpos and vpos <= (by + br)) then
         if (current_state = FIN) then
-          col <= "100";
+          col <= "111100000000";
         else
-          col <= "110";
+          col <= "111111110000";
         end if;
       elsif ((p1x - pw) <= hpos and hpos <= (p1x + pw) and (p1y - ph) <= vpos and vpos <= (p1y + ph)) then
-        col <= "100";
+        col <= "111100000000";
       elsif ((p2x - pw) <= hpos and hpos <= (p2x + pw) and (p2y - ph) <= vpos and vpos <= (p2y + ph)) then
-        col <= "001";
+        col <= "000000001111";
       else
         gx <= hpos / block_size;
         gy <= vpos / block_size;
@@ -180,16 +181,39 @@ architecture Behavioral of VideoGame is
 
           cell <= grid(gy, gx);
           if (is_face = 1) then
-            cell <= grid_face(gy, gx);
+            cell <= grid_face(15 - 1 - gy, 20 - 1 - gx) * winner;
           end if;
 
-          if (cell = 1) then
-            col <= "111";
-          else
-            col <= "010";
-          end if;
+          case cell is
+            when 0 =>
+              if ((gx + gy) mod 2 = 0) then
+                col <= "000011110000";
+              else
+                col <= "000010110000";
+              end if;
+            when 1 => 
+              col <= "111111111111";
+            when 2 => 
+              if ((gx + gy) mod 2 = 0) then
+                col <= "000011110000";
+              else
+                col <= "000010110000";
+              end if;
+            when 3 => 
+              col <= "000000001111";
+            when 4 =>
+              if ((gx + gy) mod 2 = 0) then
+                col <= "000011110000";
+              else
+                col <= "000010110000";
+              end if;
+            when 6 =>
+              col <= "111100000000";
+            when others =>
+              col <= "000000000000";
+          end case;
         else 
-          col <= "000";
+          col <= "000000000000";
         end if;
 
 
@@ -238,58 +262,60 @@ architecture Behavioral of VideoGame is
           end if;
 
           -- paddle restrictions
-          if ((p1y + ph) > 480 - (block_size * 2)) then
-            p1y <= 480 - (block_size * 2) - ph;
-          elsif ((p1y - ph) < (block_size * 2)) then
-            p1y <= (block_size * 2) + ph;
+          if ((p1y + ph + vel) > 480 - (block_size * 2)) then
+            p1y <= 480 - (block_size * 2) - ph - vel;
+          elsif ((p1y - ph - vel) < (block_size * 2)) then
+            p1y <= (block_size * 2) + ph + vel;
           end if;
 
-          if ((p2y + ph) > 480 - (block_size * 2)) then
-            p2y <= 480 - (block_size * 2) - ph;
-          elsif ((p2y - ph) < (block_size * 2)) then
-            p2y <= (block_size * 2) + ph;          
+          if ((p2y + ph + vel) > 480 - (block_size * 2)) then
+            p2y <= 480 - (block_size * 2) - ph - vel;
+          elsif ((p2y - ph - vel) < (block_size * 2)) then
+            p2y <= (block_size * 2) + ph + vel;          
           end if;
+
+          -- ball movement
+          bx <= bx + bx_dir;
+          by <= by + by_dir;
           
           -- ball-paddle collision
-          if ((p1x - pw) <= (bx - br) and (bx - br) <= (p1x + pw) and (p1y - ph) <= (by + br) and (by - br) <= (p1y + ph)) then
-            bx_dir <= vel;
-          end if;
-
-          if ((p2x - pw) <= (bx + br) and (bx + br) <= (p2x + pw) and (p2y - ph) <= (by + br) and (by - br) <= (p2y + ph)) then
-            bx_dir <= -vel;
-          end if;
-                 
-          -- ball-wall collision
-          if (grid((by + br) / block_size, (bx - br - offset) / block_size) = 1 or grid((by - br) / block_size, (bx - br - offset) / block_size) = 1) then
+          if ((p1x - pw) <= (bx - br - vel) and (bx - br - vel) <= (p1x + pw) and (p1y - ph) <= (by + br + vel) and (by - br - vel) <= (p1y + ph)) then
             bx_dir <= vel;
             bx <= bx + vel;
           end if;
-          if (grid((by + br) / block_size, (bx + br + offset) / block_size) = 1 or grid((by - br) / block_size, (bx + br + offset) / block_size) = 1) then
+
+          if ((p2x - pw) <= (bx + br + vel) and (bx + br + vel) <= (p2x + pw) and (p2y - ph) <= (by + br + vel) and (by - br - vel) <= (p2y + ph)) then
             bx_dir <= -vel;
             bx <= bx - vel;
           end if;
-          if (grid((by - br - offset) / block_size, (bx + br) / block_size) = 1 or grid((by - br - offset) / block_size, (bx - br) / block_size) = 1) then
+                 
+          -- ball-wall collision
+          if (grid((by + br) / block_size, (bx - br - vel) / block_size) = 1 or grid((by - br) / block_size, (bx - br - vel) / block_size) = 1) then
+            bx_dir <= vel;
+            bx <= bx + vel;
+          end if;
+          if (grid((by + br) / block_size, (bx + br + vel) / block_size) = 1 or grid((by - br) / block_size, (bx + br + vel) / block_size) = 1) then
+            bx_dir <= -vel;
+            bx <= bx - vel;
+          end if;
+          if (grid((by - br - vel) / block_size, (bx + br) / block_size) = 1 or grid((by - br - vel) / block_size, (bx - br) / block_size) = 1) then
             by_dir <= vel;
-            by <= bx + vel;
+            by <= by + vel;
           end if;
-          if (grid((by + br + offset) / block_size, (bx + br) / block_size) = 1 or grid((by + br + offset) / block_size, (bx - br) / block_size) = 1) then
+          if (grid((by + br + vel) / block_size, (bx + br) / block_size) = 1 or grid((by + br + vel) / block_size, (bx - br) / block_size) = 1) then
             by_dir <= -vel;
-            by <= bx - vel;
+            by <= by - vel;
           end if;
-			 
-			 -- ball movement
-          bx <= bx + bx_dir;
-          by <= by + by_dir;
 
           -- score
           if (grid((by + br) / block_size, (bx - br) / block_size) = 2 or grid((by - br) / block_size, (bx - br) / block_size) = 2) then
             current_state <= FIN;
-            winner <= '0';
+            winner <= 1;
           end if;
 
           if (grid((by + br) / block_size, (bx + br) / block_size) = 2 or grid((by - br) / block_size, (bx + br) / block_size) = 2) then
             current_state <= FIN;
-            winner <= '1';
+            winner <= 2;
           end if;
 
         when FIN =>
--- color_gen.vhd
+++ color_gen.vhd
@@ -5,7 +5,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity color_gen is
   port (
     clk : in std_logic;
-    col : in std_logic_vector(3 downto 0);
+    col : in std_logic_vector(11 downto 0);
     Rout : out std_logic_vector(7 downto 0);
     Bout : out std_logic_vector(7 downto 0);
     Gout : out std_logic_vector(7 downto 0)
@@ -17,10 +17,10 @@ begin
   process(clk)
   begin
     if (clk'Event and clk = '1') then
-      Rout <= (others => col(3));
-      Gout <= (others => col(2));
-      Bout <= (others => col(1));
-        end if;
+      Rout <= col(11) & col(11) & col(10) & col(10) & col(9) & col(9) & col(8) & col(8);
+      Gout <= col(7) & col(7) & col(6) & col(6) & col(5) & col(5) & col(4) & col(4);
+      Bout <= col(3) & col(3) & col(2) & col(2) & col(1) & col(1) & col(0) & col(0);
+    end if;
   end process;
 
 end Behavioral;
